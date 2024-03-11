8c892aa66eebee146db0a0fa53f125435e3ffd36
tommsojer <tom.sojer2@gmail.com>
2024-03-11 21:29:07 +0100

    Add side panel

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -26,6 +26,27 @@ const { subscription: authListener } = supabase.auth.onAuthStateChange(
 </script>
 
 <template>
-  <h1 class="text-lg underline">Hello world!dfas</h1>
-  <button @click="signOut">Sign out</button>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        class="bg-deep-purple"
+        theme="dark"
+        permanent
+      >
+        <v-list color="transparent">
+          <v-list-item prepend-icon="mdi-domain" title="Organizations"></v-list-item>
+          <v-list-item prepend-icon="mdi-view-dashboard" title="Projects"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-box" title="Users"></v-list-item>
+        </v-list>
+
+        <template v-slot:append>
+          <div class="pa-2">
+            <v-btn block @click="signOut">
+              Logout
+            </v-btn>
+          </div>
+        </template>
+      </v-navigation-drawer>
+    </v-layout>
+  </v-card>
 </template>
